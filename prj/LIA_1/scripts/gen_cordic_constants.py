710820a36e1be2a79bdfd2b7c80b216afd49d3f5
Fabian Vetter <fabian.vetter@hotmail.ch>
2026-09-06 13:54:56 +0200

    adapted generator python file too

--- prj/LIA_1/scripts/gen_cordic_constants.py
+++ prj/LIA_1/scripts/gen_cordic_constants.py
@@ -8,25 +8,27 @@ output_path.parent.mkdir(parents=True, exist_ok=True)
 
 N_STAGES    = 16
 PHASE_WIDTH = 32
-ANGLE_WIDTH = PHASE_WIDTH - 2   # bits representing angle 0 .. pi/2 in radians (first two bits are for quadrant)
 OUT_WIDTH   = 14
 GUARD_BITS  = 4                 # extra internal precision due to rounding errors after N_stages of CORDIC
 WORK_WIDTH  = OUT_WIDTH + GUARD_BITS
 
-# Calc of atan LUT: arctan(2^-i) table, in "quadrant units" with 2**ANGLE_WIDTH == pi/2
-# These units are needed for hardware to stay entirely in integer phase units
+# Calc of atan LUT: arctan(2^-i) table, accounting for size of z.
+# scale atan values such that largest signed WORKING_WIDTH bits integer is equal to pi/2!
 atan_vals = np.arctan(2.0 ** -np.arange(N_STAGES))
-# print(atan_vals)
-atan_scaled = np.round(atan_vals / (np.pi / 2) * (2 ** ANGLE_WIDTH)).astype(int)
+atan_scaled = np.round(atan_vals / (np.pi / 2) * (2 ** (WORK_WIDTH - 1))).astype(int)
+# take 2**(width-1) as weight and not 2**(width-1) -1 as weight although is largest 2compl number
+# -> Can see it as inheritance from phase. bit k of phase always carries a weight of (360°/2^32) × 2^k. phase[31] is 180 degrees. 
+#    Look at sign bit of z, i.e. phase[30] then this has weight of 90 degrees. So need to scale to that bit, i.e. 2^(WORK_WIDTH-1)
+# -> Can also see this since pi/2 and -pi/2 are given by 10...0 and thus are the smallest negative number in twos complement (i.e. -2^(WORK_WIDTH-1))
+#    So scale to that, then our range is [-pi/2,pi/2)!
 
 with open(output_path, "w") as f:
     for v in atan_scaled:
-        f.write(f"{v & (2**(ANGLE_WIDTH+1) - 1):0{(ANGLE_WIDTH+1+3)//4}x}\n")
-        # v & (2**(ANGLE_WIDTH+1) - 1) -> bitwise AND with 0000111...1 with Angle_width+1 many 1s to make the python integer v fixed widt (angle +1)
-        # want angle_width+1 because z angle also needs to go negative, i.e. cover plus minus pi/2 range! Only subangle is width angle_wdth (0,pi/2 range)
-        # :0{(ANGLE_WIDTH+1+3)//4}x specifies Python's f-string format specification. x at the end makes it hexadecimal rather than default decimal.
-        # 0<N> -> zero pad output on the left to exactly N characters wide, st in file every number has same amount of hex digits, , so Verilog's file reader can consistently map each line onto one ANGLE_WIDTH+1-bit array entry
-        # {(ANGLE_WIDTH+1+3)//4} -> number of hex digits needed to represent ANGLE_WIDTH+1 bits. 
+        f.write(f"{v & (2**WORK_WIDTH - 1):0{(WORK_WIDTH+3)//4}x}\n")
+        # v & (2**WORK_WIDTH - 1) -> bitwise AND with 0000111...1 with WORK_WIDTH many 1s to make the python integer v fixed width (WORK_WIDTH)
+        #                            this is exactly the size of z in the CORDIC stages!
+        # :0{(WORK_WIDTH+3)//4}x  -> specifies Python's f-string format specification. x at the end makes it hexadecimal rather than default decimal.
+        # {(WORK_WIDTH+3)//4}x}   -> number of hex digits needed to represent WORK_WIDTH bits.    
 
 # Calcualte CORDIC gain and the gain-precorrected initial x
 K = np.prod(np.cos(atan_vals[:N_STAGES]))
