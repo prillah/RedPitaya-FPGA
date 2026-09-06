b2d7e409cb35e3921278199725c223a6e7166f59
Fabian Vetter <fabian.vetter@hotmail.ch>
2026-09-06 16:12:29 +0200

    added reset test for cordic

--- prj/LIA_1/tbn/test_cordic_sincos.py
+++ prj/LIA_1/tbn/test_cordic_sincos.py
@@ -26,11 +26,48 @@ async def drive_and_read(dut, theta_deg: float):
     Need async def here, becuase this function should be coroutine, i.e. being able to wait for HDL simulator feedback before continuing sequentially."""
 
     dut.phase.value = deg_to_phase(theta_deg)   # syntax for adressing the phase input value!
-    for _ in range(LATENCY):
+    for _ in range(LATENCY):                    # let system run
         await RisingEdge(dut.clk)
-    cos_raw = dut.cos_o.value.signed_integer
+    cos_raw = dut.cos_o.value.signed_integer    # interpret value as signed integer in python. In cocotb return type of dut.q.value is LogicArray or BinaryArray and not binary number/integer.
     sin_raw = dut.sin_o.value.signed_integer
-    return cos_raw / FULL_SCALE, sin_raw / FULL_SCALE
+    return cos_raw / FULL_SCALE, sin_raw / FULL_SCALE   # rescale into [-1,+1] range
+
+
+
+@cocotb.test()
+async def test_reset(dut):
+    """Test if reset clears stage 0 (i.e. x[0], y[0], z[0]) synchronously,
+    also if real, non-zero data already loaded into the pipeline registers."""
+
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())    # coroutine, schedules it to run in the background alongside main test function -> otherwise testbench gets forever stuck here
+    dut.reset.value = 0
+
+    # fill stage 0 with non zero data
+    dut.phase.value = deg_to_phase(137.0)
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+
+    assert dut.x[0].value.signed_integer != 0 or dut.z[0].value.signed_integer != 0, (
+        "Error: stage 0 didn't actually load data properly!"
+    )
+
+    dut.reset.value = 1
+    await RisingEdge(dut.clk)
+    dut.reset.value = 0
+
+    assert dut.x[0].value.signed_integer == 0, "x[0] did not clear on reset"
+    assert dut.y[0].value.signed_integer == 0, "y[0] did not clear on reset"
+    assert dut.z[0].value.signed_integer == 0, "z[0] did not clear on reset"
+
+    dut._log.info("Reset correctly clears stage 0.")
+
+
+
+@cocotb.text()
+async def test_static_angles(dut):
+    """
+    
+    """
 
 
 
