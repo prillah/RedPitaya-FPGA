30699d2e320ba61d32af124101a733dfaefbe0a2
Fabian Vetter <fabian.vetter@hotmail.ch>
2026-09-09 22:34:04 +0200

    small changes to test file

---
+++ prj/LIA_1/tbn/test_dds_cordic.py
@@ -0,0 +1,42 @@
+# /tbn/test_dds_cordic.py
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
