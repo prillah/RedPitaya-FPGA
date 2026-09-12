e8ac2403d24d6e59aa4023afcdcd885e5f435148
Fabian Vetter <fabian.vetter@hotmail.ch>
2026-09-12 14:10:45 +0200

    added reset test for dds module bench

--- prj/LIA_1/tbn/test_dds_cordic.py
+++ prj/LIA_1/tbn/test_dds_cordic.py
@@ -18,14 +18,31 @@ async def reset_dut(dut):   # coroutine to reset the DUT in every test
     await Timer(1, unit="ns")
     dut.reset.value = 0
 
-
 @cocotb.test()
 async def test_reset(dut):
     """
     Test if set phase_acc to 0 with reset signal
     """
 
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
+
+    # instantiate the module with some finite value
+    dut.ftw.value = 12345
+    dut.reset.value = 0
+    for _ in range(5):
+        await RisingEdge(dut.clk)
+    assert dut.phase_acc.value.integer != 0, "setup issue: phase_acc never advanced/changed"
+
+    await reset_dut(dut)
+
+    assert dut.phase_acc.value.integer == 0, "phase_acc not cleared on reset"
+    dut._log.info("phase_acc correctly cleared on reset")
+
+@cocotb.test()
+async def test_dds(dut):
+    """
     
+    """
 
 
 
