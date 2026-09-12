95112dda5d7840e09325205c850d87b968c32c3f
Fabian Vetter <fabian.vetter@hotmail.ch>
2026-09-12 13:57:33 +0200

    some changes to test function of dds

--- prj/LIA_1/tbn/test_dds_cordic.py
+++ prj/LIA_1/tbn/test_dds_cordic.py
@@ -6,7 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# setup parameters for cordic module as well as some for python simulation
+PHASE_WIDTH = 32
+N_STAGES    = 16
+OUT_WIDTH   = 14
+LATENCY     = N_STAGES + 1  # cycles needed from phase input to valid output
 
+async def reset_dut(dut):   # coroutine to reset the DUT in every test
+    dut.reset.value = 1
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
+    dut.reset.value = 0
+
+
+@cocotb.test()
+async def test_reset(dut):
+    """
+    Test if set phase_acc to 0 with reset signal
+    """
+
+    
 
 
 
